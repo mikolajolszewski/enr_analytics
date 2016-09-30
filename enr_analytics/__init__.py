edbda66c9c9bea0abae8f2ad7421af7dfed0cc1a
Mikolaj Olszewski <mikolaj.olsz@gmail.com>
2016-09-30 12:56:00 +0200

    Fix bugs

--- enr_analytics/__init__.py
+++ enr_analytics/__init__.py
@@ -1,5 +1,7 @@
+import matplotlib
+
 # Define functions removing right and top border chartarea border
-def removeLines() :
+def removeLines(ax) :
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.yaxis.set_ticks_position('left')
@@ -7,10 +9,10 @@ def removeLines() :
 
 # Define function saving a plot
 def savePlot(path) :
-    plt.savefig(path + '.png', bbox_inches = 'tight', dpi = 300)
-    plt.savefig(path + '.pdf', bbox_inches = 'tight')
+    matplotlib.pyplot.savefig(path + '.png', bbox_inches = 'tight', dpi = 300)
+    matplotlib.pyplot.savefig(path + '.pdf', bbox_inches = 'tight')
 
 # Set matplotlib template
 def initialChartConfig() :
-    plt.style.use('https://raw.githubusercontent.com/mikolajolszewski/end_analytics/master/enr_analytics/enr_analytics.mplstyle')
+    matplotlib.pyplot.style.use('https://raw.githubusercontent.com/mikolajolszewski/end_analytics/master/enr_analytics/enr_analytics.mplstyle')
     matplotlib.rcParams['font.family'] = 'Open Sans'
